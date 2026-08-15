39c86f27262f33a69c7d940b08f6ff0971900e2f
Murilo Pereira <134763300+Bl4ast@users.noreply.github.com>
2026-08-15 00:34:49 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,31 @@
 const config = {
-    images: {
-        wallpaper: "assets/Back.gif",
-        profilePic: "assets/Icon.jpg"
+    currentTheme: "Hanako",
+    themes: {
+        hanako: {
+            name: "Hanako",
+            bgDark: "#050505",
+            panelBg: "rgba(15, 15, 15, 0.75)",
+            textMain: "#f0ecec",
+            textMuted: "#998888",
+            redAccent: "#da0f20",
+            greenAccent: "#2e8b57",
+            borderColor: "#c51524",
+            wallpaper: "assets/Back.gif",
+            profilePic: "assets/Icon.jpg"
+        },
+        vanitas: {
+            name: "Vanitas",
+            bgDark: "#020205",
+            panelBg: "rgba(10, 10, 20, 0.8)",
+            textMain: "#e0fbfc",
+            textMuted: "#031855",
+            redAccent: "#03486d",
+            greenAccent: "#013776",
+            borderColor: "#0d1380",
+            wallpaper: "assets/Livro.jpg",
+            profilePic: "assets/VIcon.jpg"
+        },
+        
     },
     texts: {
         badgeProfile: "BL4AST",
@@ -24,12 +48,12 @@ const config = {
         badgeProd: "Produtividade:",
         titleTasks: "Tarefas do Dia",
         btnTaskAdd: "Criar Tarefa Extra",
-        labelToday: "",
-        phFinance: "Ex: Salário, Conta...",
-        phComm: "Comm",
+        labelToday: "Rotina Automática & Compromissos",
+        phFinance: "Descrição",
+        phComm: "Comm...",
         phAgenda: "Compromisso...",
         phCollege: "Trabalho / Prova...",
         phRoutine: "Nova rotina fixa...",
-        phTask: "Adicionar tarefa extra hoje..."
+        phTask: "Adicionar tarefa"
     }
 };
